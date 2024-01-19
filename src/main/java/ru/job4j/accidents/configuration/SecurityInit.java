38907b41bda59805119fd7ebff66c70527726206
WoWheaded <d-343@yandex.ru>
2024-01-19 15:22:33 +0300

    0. Spring Security [#6879]

---
+++ src/main/java/ru/job4j/accidents/configuration/SecurityInit.java
@@ -0,0 +1,6 @@
+package ru.job4j.accidents.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityInit extends AbstractSecurityWebApplicationInitializer {
+}
