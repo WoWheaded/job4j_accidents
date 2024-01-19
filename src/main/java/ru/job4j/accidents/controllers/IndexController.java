b36307e4f8ce45a5b85ca8da0c484dcdff933c42
WoWheaded <d-343@yandex.ru>
2024-01-19 15:35:14 +0300

    1. Авторизация через JDBC [#2094]

--- src/main/java/ru/job4j/accidents/controllers/IndexController.java
+++ src/main/java/ru/job4j/accidents/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package ru.job4j.accidents.controllers;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +15,7 @@ public class IndexController {
 
     @GetMapping({"/", "/accidents"})
     public String getAllAccidents(Model model) {
-        model.addAttribute("user", "Dmitry Arinin");
+        model.addAttribute("user", SecurityContextHolder.getContext().getAuthentication().getPrincipal());
         model.addAttribute("accidents", simpleAccidentService.findAllAccidents());
         return "index";
     }
